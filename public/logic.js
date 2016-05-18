06ffb372acb8b2f1f10f796d74c8849ed1508675
jitendrs@thoughtworks.com <jitendrs@thoughtworks.com>
2016-05-19 02:54:03 +0530

    2048 game implimentation started

---
+++ public/logic.js
@@ -0,0 +1,125 @@
+var tiles = [
+	["","","",""],
+	["","","",""],
+	["","","",""],
+	["","","",""]
+]
+
+var possibleNumbers = {
+	0 : 2,
+	1 : 4
+}
+
+var fill = function() {
+	var currentTiles = document.querySelectorAll("input");
+	var i = 0;
+	tiles.forEach(function(tileRow){
+		tileRow.forEach(function(tile){
+			currentTiles[i].setAttribute("value", tile);
+			i++;
+		});
+	});
+};
+
+var getPossiotion = function() {
+	while(true){
+		var x = Math.floor(Math.random() * 4);
+		var y = Math.floor(Math.random() * 4);
+		if(tiles[x][y] == "") return [x, y];
+	}
+}
+
+var initialize = function() {
+	var x = Math.floor(Math.random() * 4);
+	var y = Math.floor(Math.random() * 4);
+	tiles[0][0] = 2;
+	tiles[y][x] = possibleNumbers[Math.floor(Math.random() * 2)];
+	fill();
+};
+
+var canGo = function(x, y, direction){
+	if(direction == "down")
+		return ((x!=3) && (tiles[x+1][y]=="" || tiles[x+1][y]==tiles[x][y]))
+	if(direction == "up")
+		return ((x!=0) && (tiles[x-1][y]=="" || tiles[x-1][y]==tiles[x][y]))
+	if(direction == "right")
+		return ((y!=3) && (tiles[x][y+1]=="" || tiles[x][y+1]==tiles[x][y]))
+	if(direction == "left")
+		return ((y!=0) && (tiles[x][y-1]=="" || tiles[x][y-1]==tiles[x][y]))
+}
+
+var directions = {
+	"down" : function(x, y){
+		tiles[x+1][y] = (tiles[x+1][y]=="") ? tiles[x][y] : tiles[x+1][y]+tiles[x][y];
+	},
+
+	"up" : function(x, y){
+		tiles[x-1][y] = (tiles[x-1][y]=="") ? tiles[x][y] : tiles[x-1][y]+tiles[x][y];
+	},
+	"right" : function(x, y){
+		tiles[x][y+1] = (tiles[x][y+1]=="") ? tiles[x][y] : tiles[x][y+1]+tiles[x][y];
+	},
+	"left" : function(x, y){
+		tiles[x][y-1] = (tiles[x][y-1]=="") ? tiles[x][y] : tiles[x][y-1]+tiles[x][y];
+	}
+}
+
+var generateNumber = function(){
+	var possition = getPossiotion();
+	tiles[possition[0]][possition[1]] = possibleNumbers[Math.floor(Math.random() * 2)];
+	fill();
+}
+
+var down = function() {
+	for (var i = 0; i < tiles.length; i++) {
+		for (var j = 0; j < tiles[i].length-1; j++) {
+			if(canGo(j, i, "down")){
+				directions.down(j, i, "down");
+				tiles[x][y] = "";
+			}
+		}
+	}
+	generateNumber();
+}
+
+var up = function() {
+	for (var i = tiles.length; i >= 0; i--) {
+		for (var j = tiles.length; j >= 0; j--) {
+			if(canGo(j, i, "up")){
+				directions.up(j, i);
+				tiles[x][y] = "";
+			}
+		}
+	}
+	generateNumber();
+}
+
+
+var right = function() {
+	for (var i = 0; i < tiles.length; i++) {
+		for (var j = 0; j < tiles[i].length-1; j++) {
+			if(canGo(i, j, "right")){
+				directions.right(i, j, "right");
+				tiles[x][y] = "";
+			}
+		}
+	}
+	generateNumber();
+}
+
+var left = function() {
+	for (var i = tiles.length; i >= 0; i--) {
+		for (var j = tiles.length; j >= 0; j--) {
+			if(canGo(i, j, "left")){
+				directions.left(i, j, "left");
+				tiles[x][y] = "";
+			}
+		}
+	}
+	generateNumber();
+}
+
+window.onload = initialize();
+
+
+
---
+++ server/server.js
@@ -0,0 +1,15 @@
+var http = require("http");
+var fs = require("fs");
+var handlers = function(req, res){
+	if(req.url == "/"){
+		req.url = "/index.html";
+	}
+	if(fs.existsSync("../public"+req.url)){
+		var file = fs.readFileSync("../public"+req.url);
+		res.end(file);
+	}else{
+		res.end();
+	}
+}
+
+http.createServer(handlers).listen(8000);
