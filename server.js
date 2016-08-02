b7f7eaa49194e0b1085e0818843a257e8ebdd152
Ganesh Patil <ganeshpl@thoughtworks.com>
2016-08-02 18:00:53 +0530

    Printserver port on the console

--- server.js
+++ server.js
@@ -20,4 +20,6 @@ app.use(express.static('public/.'));
 app.get('/', function(req, res){
 	res.redirect('index.html');
 });
-http.createServer(app).listen(process.env.OPENSHIFT_NODEJS_PORT || 3000, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
+var port = process.env.OPENSHIFT_NODEJS_PORT || 3000;
+console.log("Starting Server at port:", port);
+http.createServer(app).listen(port, process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1");
